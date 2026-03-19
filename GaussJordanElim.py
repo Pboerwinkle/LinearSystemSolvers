88b11f09e1401e01b556f12c66c914292eca9c7a
Pboerwinkle <120537997+Pboerwinkle@users.noreply.github.com>
2026-03-19 14:41:04 -0500

    Update GaussJordanElim.py to accept separate b array

--- GaussJordanElim.py
+++ GaussJordanElim.py
@@ -1,6 +1,6 @@
 import numpy as np
-matrix = np.array([[3,2,-1,1],[2,-2,4,-2],[-1,0.5,-1,0]])
-def elim(matrix):
+def elim(matrix, b):
+    matrix=np.hstack((matrix, b.T))
     for row in range(len(matrix)):
         for row2 in range(row+1,len(matrix)):
             matrix[row2]=matrix[row2] - (matrix[row2][row]/matrix[row][row])*matrix[row]
@@ -10,4 +10,4 @@ def elim(matrix):
     for row in range(len(matrix)):
         matrix[row]/=matrix[row][row]
     return matrix
-print(elim(matrix))
+print(elim(np.array([[2,1,-1],[-3,-1,2],[-2,1,2.0]]), np.array([[8, -11, -3.0]])))
